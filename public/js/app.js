b326db0f1965bbc7b2ad0ed08cb4792c47fefbf0
Turbanov Valeriy <tur.4000@gmail.com>
2019-12-03 22:51:02 +0200

    install

--- public/js/app.js
+++ public/js/app.js
@@ -49592,8 +49592,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\USR\OpenServer\OSPanel\domains\laravel\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\USR\OpenServer\OSPanel\domains\laravel\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/tur/prj/laravel/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/tur/prj/laravel/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
