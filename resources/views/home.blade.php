e40d4bd3b34297a4adf5d140d28d3d1299c13cd2
Turbanov Valeriy <tur.4000@gmail.com>
2019-12-03 23:04:51 +0200

    front

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    Your site!
                 </div>
             </div>
         </div>
