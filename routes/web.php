c769221921c373f44f2fecb618319a9698419832
Turbanov Valeriy <tur.4000@gmail.com>
2019-12-03 23:01:38 +0200

    front

--- routes/web.php
+++ routes/web.php
@@ -10,15 +10,13 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+//
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/cabinet', 'Cabinet\HomeController@index')->name('cabinet');
